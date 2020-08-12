40f947a7a4bdc9f6c5e79e8e4f0a6cf0c12992e2
aleksandrgaranin <aleksandrgaranin89@gmail.com>
2020-08-12 15:00:18 -0700

    Using Refs with Hocs

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,14 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import classes from './Cockpit.module.css'
 
 const Cockpit = props => {
+    const toggleBtnRef = useRef(null);
+    
 
     useEffect(()=> {
         console.log('[Cockpit.js] useEffect');
         //Http request...
-        setTimeout(()=>{
-            alert('Saved data to cloud!');
-        }, 1000);
+        // setTimeout(()=>{
+        //     alert('Saved data to cloud!');
+        // }, 1000);
+        toggleBtnRef.current.click();
         return ()=>{            
             console.log('[Cocpit.js] clean up work in useEffect');
         };
@@ -42,7 +45,8 @@ const Cockpit = props => {
             <p className={assinedClasses.join(' ')}>
                 This is working
             </p>
-            <button 
+            <button
+            ref={toggleBtnRef} 
             className={btnClass}
             onClick={props.clicked}>
                 Toggle Persons
